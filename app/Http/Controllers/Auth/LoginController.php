8f69563ad42404815fac506e8eaf086a634cadf6
bull0n <lucas.bulloni@protonmail.ch>
2018-10-01 09:01:14 +0200

    seeder for users, prepare laravel for ajax login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,33 +7,51 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+  /*
+  |--------------------------------------------------------------------------
+  | Login Controller
+  |--------------------------------------------------------------------------
+  |
+  | This controller handles authenticating users for the application and
+  | redirecting them to your home screen. The controller uses a trait
+  | to conveniently provide its functionality to your applications.
+  |
+  */
 
-    use AuthenticatesUsers;
+  use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+  /**
+  * Where to redirect users after login.
+  *
+  * @var string
+  */
+  protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+  /**
+  * Create a new controller instance.
+  *
+  * @return void
+  */
+  public function __construct()
+  {
+    $this->middleware('guest')->except('logout');
+  }
+
+  public function login(Request $request)
+  {
+    $credentials = $request->only($this->username(), 'password');
+    $authSuccess = Auth::attempt($credentials, $request->has('remember'));
+
+    if($authSuccess)
     {
-        $this->middleware('guest')->except('logout');
+      $request->session()->regenerate();
+      return response(['success' => true], Response::HTTP_OK)->json('redirectTo' => $this->redirectTo);
     }
+
+    return response(
+    [
+      'success' => false,
+      'message' => 'Auth failed (or some other message)'
+    ], Response::HTTP_FORBIDDEN);ata
+  }
 }

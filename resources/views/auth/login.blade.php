45392f24de3f862da1aa55fa48f3b5d43fbf1b2e
bull0n <lucas.bulloni@protonmail.ch>
2018-10-05 12:11:03 +0200

    add a script for registering

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,34 +5,44 @@
 @endsection
 
 @section('content')
-<div class="form-signin" id="login-form">
-  <form method="post" action="{{ route('login') }}" v-on:submit.prevent="submit">
-    @csrf
+<div class="form-signin container" id="login-form">
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+      <form method="post" action="{{ route('login') }}" v-on:submit.prevent="submit">
+        @csrf
 
-    <h1 class="h3 mb-3 font-weight-normal">{{ __('Login') }}</h1>
+        <h1 class="h3 mb-3 font-weight-normal">{{ __('Login') }}</h1>
 
-    <b-alert :show="!valid" fade dismissible variant="danger">{{ __('The email or password is not correct.') }}</b-alert>
+        <div class="alert alert-danger" role="alert" v-show="!valid">
+          {{ __('The email or password is not correct.') }}
+        </div>
 
-    <label for="email" class="sr-only">{{ __('E-Mail Address') }}</label>
-    <input type="email" id="email" class="form-control" placeholder="{{ __('E-Mail Address') }}" v-model="email" required autofocus>
+        <label for="email" class="sr-only">{{ __('E-Mail Address') }}</label>
+        <input type="email" id="email" class="form-control" placeholder="{{ __('E-Mail Address') }}" v-model="email" required autofocus>
 
-    <label for="password" class="sr-only">{{ __('Password') }}</label>
-    <input type="password" id="password" class="form-control" placeholder="{{ __('Password') }}" v-model="password" required>
+        <label for="password" class="sr-only">{{ __('Password') }}</label>
+        <input type="password" id="password" class="form-control" placeholder="{{ __('Password') }}" v-model="password" required>
 
-    <div class="custom-control custom-checkbox mb-3">
-      <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} v-model="remember">
-      <label class="custom-control-label" for="remember">
-         {{ __('Remember Me') }}
-      </label>
+        <div class="custom-control custom-checkbox mb-3">
+          <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} v-model="remember">
+          <label class="custom-control-label" for="remember">
+             {{ __('Remember Me') }}
+          </label>
+        </div>
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit">{{ __('Login') }}</button>
+
+        <div class="text-right">
+          <a href="{{ route('password.request') }}">
+            {{ __('Forgot Your Password?') }}
+          </a>
+          -
+          <a href="{{ route('register') }}">
+            {{ __('Register') }}
+          </a>
+        </div>
+      </form>
     </div>
-
-    <button class="btn btn-lg btn-primary btn-block" type="submit">{{ __('Login') }}</button>
-
-    <div class="text-right">
-    <a class="btn btn-link" href="{{ route('password.request') }}">
-        {{ __('Forgot Your Password?') }}
-    </a>
   </div>
-  </form>
 </div>
 @endsection

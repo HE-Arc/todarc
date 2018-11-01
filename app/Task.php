a52e23335982d616d7d804f20ec71ba21873e475
Asmoday4949 <malik.fleury@he-arc.ch>
2018-11-01 15:47:54 +0100

    Added name as readable attribute

--- app/Task.php
+++ app/Task.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    /**
      * The labels that belong to the task.
      */
     public function labels()

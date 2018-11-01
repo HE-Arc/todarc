8db5976497380a17b08d71e2618c52ca9d47e6a0
bull0n <lucas.bulloni@protonmail.ch>
2018-11-01 17:09:00 +0100

    add a component for adding an organization

---
+++ resources/js/components/UserDashboard/AddNewOrg.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="btn btn-primary btn-lg" href="#" role="button" v-on:click="clicked">{{ buttonText }}</button>
+</template>
+
+<script>
+  module.exports =
+  {
+    props:
+    {
+      buttonText: String
+    },
+    methods:
+    {
+      clicked: function()
+      {
+        console.log('ici');
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/js/components/UserDashboard/Organisation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card text-center">
+    <h4 class="card-header">{{ name }}</h4>
+    <div class="card-body">
+      <h6>Members : </h6>
+      <ul>
+        <li v-for="member in members">{{member}}</li>
+      </ul>
+    </div>
+    <div class="card-footer">
+      <a v-bind:href="uriOrganisation"><button class="btn btn-light btn-block">{{buttonText}}</button></a>
+    </div>
+  </div>
+</template>
+
+<script>
+  module.exports =
+  {
+    props:
+    {
+      name: String,
+      buttonText: String,
+      uriOrganisation: String,
+      members: Array
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

b47d7c2e0fdcd979d3a32eecc77c7a9b1e25ca05
bull0n <lucas.bulloni@protonmail.ch>
2018-11-04 16:28:09 +0100

    add vue component for the button to add a project and  an organisation

---
+++ resources/js/components/UserDashboard/AddNewProject.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="btn btn-primary btn-lg" href="#" role="button" v-on:click="clicked">{{ buttonText }}</button>
+</template>
+
+<script>
+  module.exports =
+  {
+    data: function ()
+    {
+      return {
+        projectName: '',
+        projectDescription: ''
+      }
+    },
+    props:
+    {
+      buttonText: String
+    },
+    methods:
+    {
+      clicked: function()
+      {
+        console.log(this.projectName);
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

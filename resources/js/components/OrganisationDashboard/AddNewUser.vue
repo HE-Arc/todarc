f98cea792a25c937e9ee028cadae83be8cec2c76
malik <malik.fleury@he-arc.ch>
2018-11-14 20:38:13 +0100

    Added new components for users and projects

---
+++ resources/js/components/OrganisationDashboard/AddNewUser.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button class="btn btn-primary btn-lg" href="#" role="button" v-on:click="newUser">{{ buttonText }}</button>
+    <modal
+      v-on:confirmed=""
+      v-model="newUser"
+      ref="modalNewUser"
+      title="New user"
+      input-label="Username"
+      data-input=""
+    ></modal>
+  </div>
+</template>
+
+<script>
+  module.exports =
+  {
+    data: function ()
+    {
+      return {
+        orgName: String,
+        orgUsers: Array(),
+        userName: String,
+      }
+    },
+    props:
+    {
+      buttonText: String
+    },
+    methods:
+    {
+      newUser: function()
+      {
+        this.$refs.modalNewUser.open();
+      },
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

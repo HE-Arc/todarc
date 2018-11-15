220f86d827e2fbf54bbced98ababb10f440d5857
bull0n <lucas.bulloni@protonmail.ch>
2018-11-15 16:51:41 +0100

    deploy script complete

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,4 @@ USERNAME=poweruser
 SERVER="srvz-webapp.he-arc.ch"
 PORT=2261
 
-ssh ${USERNAME}@${SERVER} -p${PORT}
-date
+ssh ${USERNAME}@${SERVER} -p${PORT} 'cd /var/www/app/ ; git checkout deploy ; git pull'

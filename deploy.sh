6629190946bf314c3cbda15d96296ed16fefa858
bull0n <lucas.bulloni@protonmail.ch>
2018-11-15 17:07:55 +0100

    update npm and composer

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ USERNAME=poweruser
 SERVER="srvz-webapp.he-arc.ch"
 PORT=2261
 
-ssh ${USERNAME}@${SERVER} -p${PORT} 'cd /var/www/app/ ; git checkout deploy ; git pull'
+ssh ${USERNAME}@${SERVER} -p${PORT} 'cd /var/www/app/ ; git checkout deploy ; git pull ; npm install ; composer install ; npm run production'

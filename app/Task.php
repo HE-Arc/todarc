51062255e8413e4d67eb7dc458627eb2a2bd9b70
malik <malik.fleury@he-arc.ch>
2018-11-22 10:33:36 +0100

    Fixed error

--- app/Task.php
+++ app/Task.php
@@ -15,15 +15,6 @@ class Task extends Model
     protected $fillable = ['name', 'group_id', 'from_date', 'until_date', 'description', 'order', 'done'];
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name'
-    ];
-
-    /**
      * The labels that belong to the task.
      */
     public function labels()

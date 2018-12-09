6d977acadb26975b8d215195404d37cdfe4fd554
malik <malik.fleury@he-arc.ch>
2018-12-09 16:22:25 +0100

    Added an image to main page

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,13 +7,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
 
                 <div class="card-header">Qu'est-ce que TODARC ?</div>
                 <div class="card-body">
                   <p>C'est une applicaton web permettant de gérer des todos list. Les utilisateurs peuvent gérer la liste des tâches à faire pour chacun de leurs projets.
                   Les utilisateurs peuvent être regroupés au sein d'une même entité (organisation) qui correspond à tous les membres travaillant sur le(s) projet(s) de cette entité.</p>
+                  <p>Pour avoir accès à cet outil, il est nécessaire de se loguer au site. Si vous avez déjà un compte, vous pouvez vous loguer ici. Si vous n'avez pas encore de compte,
+                    il est possible de s'inscrire ici.</p>
                 </div>
 
                 <div class="card-header">Fonctionnalités</div>
@@ -26,6 +28,11 @@
                     <li>Gestion de tâches de projet (nom de la tâche, personne responsable, dates de début et de fin, etc...)</li>
                     <li>Attribution de labels sur les tâches</li>
                   </ul>
+                </div>
+
+                <div class="card-header">TODARC en images</div>
+                <div class="card-body">
+                  <img alt="Register" src="{{ asset('images/register.png') }}" height="50%" widht="50%" align="middle"></img>
                 </div>
 
                 <div class="card-header">A Propos</div>

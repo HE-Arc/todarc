5ac5b8c31d130335276015c910bfc165d85f8e0f
bull0n <lucas.bulloni@protonmail.ch>
2018-12-12 11:05:36 +0100

    change deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,16 @@ PORT=2261
 git checkout deploy
 git merge master
 git push
-ssh ${USERNAME}@${SERVER} -p${PORT} 'cd /var/www/app/ ; git checkout deploy ; git stash ; git stash drop ; git pull ; npm install ; composer install --no-dev ; npm run production'
+ssh ${USERNAME}@${SERVER} -p${PORT} '
+cd /var/www/app/ ;
+git checkout deploy ;
+echo "stashing" ; git stash ;
+git stash drop ;
+echo "pulling" ; git pull ;
+echo "npm install" ; npm install ;
+echo "composer install" ; composer install --no-dev ;
+echo migrate ; php artisan migrate ;
+echo routecache ; php artisan route:cache ;
+echo rebuilding-sass ; npm rebuild node-sass ;
+echo npmrun ; npm run production'
+git checkout master

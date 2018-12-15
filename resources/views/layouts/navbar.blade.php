38686982767cea849122ebb9f45e5740beb69ade
bull0n <lucas.bulloni@protonmail.ch>
2018-12-15 15:56:41 +0100

    navbar mobile problem

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,14 +3,18 @@
     <a class="navbar-brand" href="{{ url('/') }}">
       <img src="{{ asset('images/logo.bmp') }}" style="max-height: 50px; margin-bottom: -20px; margin-top: -30px;" alt="{{ config('app.name', 'ToDArc') }}">
     </a>
-    <a class="nav-link" href="{{ url('/about') }}">
-      About
-    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/about') }}">
+            About
+          </a>
+        </li>
+      </ul>
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav ml-auto">
         <!-- Authentication Links -->

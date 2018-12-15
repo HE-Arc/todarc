7942c56db4f9faeb6562995c7868921296093de7
Bastien Wermeille <bastien.wermeille@gmail.com>
2018-12-15 11:45:26 +0100

    Translate about page in english

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,76 +1,84 @@
+
+
 @extends('layouts.app')
-
 @section('scripts')
-  <script src="{{ asset('js/app.js') }}" defer></script>
+<script src="{{ asset('js/app.js') }}" defer></script>
 @endsection
-
 @section('content')
 <div id="main-page" class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-
-                <div class="card-header">Qu'est-ce que TODARC ?</div>
-                <div class="card-body">
-                  <p>C'est une applicaton web permettant de gérer des todos list. Les utilisateurs peuvent gérer la liste des tâches à faire pour chacun de leurs projets.
-                  Les utilisateurs peuvent être regroupés au sein d'une même entité (organisation) qui correspond à tous les membres travaillant sur le(s) projet(s) de cette entité.</p>
-                  <p>Pour avoir accès à cet outil, il est nécessaire de se loguer au site. Si vous avez déjà un compte, vous pouvez vous loguer <a href="login">ici.</a>. Si vous n'avez pas encore de compte,
-                    il est possible de s'inscrire:</p>
-                  <a class="btn btn-outline-success" href="register">Register</a>
-                  <a class="btn btn-outline-success" href="login">Connect</a>
-                </div>
-              </div>
+   <div class="row justify-content-center">
+      <div class="col-md-12">
+         <div class="card">
+            <div class="card-header">What is TODARC ?</div>
+            <div class="card-body">
+               <p>This web application allow managing todo lists. Users can manage their own list of tasks for each one of their projects.
+                  Users can create some organisations to work in team inside a unique entity. It's possible to create a project inside an organisation and all members of this team can edit it.
+               </p>
+               <p>The usage of this amazing tools, required you be registered. If you have already created your account, you can <a class="btn btn-outline-success" href="login">log in</a>.
+                  If you're still not register down't wait and <a class="btn btn-outline-success" href="register">Register now</a>
+               </p>
             </div>
-          </div>
-          <div class="pt-3 row justify-content-center">
+         </div>
+      </div>
+   </div>
+   <div class="pt-3 row justify-content-center">
+      <div class="col-md-12">
+         <div class="card">
+            <div class="card-header">Functionalities</div>
+            <div class="card-body">
+               <p>ToD'Arc offers you the following functionalities:/p>
+               <ul>
+                  <li>Create/remove an organisation</li>
+                  <li>Create/remove project linked to a user or an organisation</li>
+                  <li>Manage members or and organisation</li>
+                  <li>Manage task for each project (name of the task, responsible, start time, due to date, etc...)</li>
+                  <li>Labels management for project</li>
+                  <li>Labels attribution to tasks</li>
+                  <li>Hierarchical organisation of tasks and groups of tasks</li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="pt-3 row justify-content-center">
+      <div class="col-md-12">
+         <div class="card">
+            <div class="card-header">TODARC in pictures</div>
+            <div class="card-body">
+               <img alt="Project-Dashboard" src="{{ asset('images/Project-Tasks.png') }}" class="centered-img"></img>
+               <img alt="User-Dashboard" src="{{ asset('images/User-Dashboard.png') }}" class="centered-img"></img>
+               <img alt="Organisation-Dashboard" src="{{ asset('images/Organisation-Dashboard.png') }}" class="centered-img"></img>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="pt-3 row justify-content-center">
+      <div class="col-md-12">
+         <div class="card">
+            <div class="card-header">About</div>
+            <div class="card-body">
+               <p>This web application has been developed during the 2018 autumn semester at "HE-Arc Ingénierie" of "Neuchâtel" as part of the course of web development.</p>
+               <p>People having contributed to this amazing project are:</p>
+               <ul>
+                  <li><a href="https://github.com/Asmoday4949">Fleury Malik</a></li>
+                  <li><a href="https://github.com/bull0n">Bulloni Lucas</a></li>
+                  <li><a href="https://github.com/Ph0tonic/">Wermeille Bastien</a></li>
+               </ul>
+            </div>
+         </div>
+         <div class="pt-3 row justify-content-center">
             <div class="col-md-12">
-              <div class="card">
-                <div class="card-header">Fonctionnalités</div>
-                <div class="card-body">
-                  <p>Notre application comporte les fonctionnalités suivantes :</p>
-                  <ul>
-                    <li>Ajout/suppression d'organisations</li>
-                    <li>Ajout/suppression de projet lié à un utilisateur ou une organisation</li>
-                    <li>Ajout/éjection de membre au sein d'une organisation</li>
-                    <li>Gestion de tâches de projet (nom de la tâche, personne responsable, dates de début et de fin, etc...)</li>
-                    <li>Attribution de labels sur les tâches</li>
-                  </ul>
-                </div>
-              </div>
+               <div class="card">
+                  <div class="card-header">Contribute</div>
+                  <div class="card-body">
+                     <p>This web application is an open source project and your contributions are welcome.</p>
+                     <p>Please visite our <a href="https://github.com/HE-Arc/todarc">Github repo</a>.</p>
+                  </div>
+               </div>
             </div>
-          </div>
-
-          <div class="pt-3 row justify-content-center">
-            <div class="col-md-12">
-              <div class="card">
-
-                <div class="card-header">TODARC en images</div>
-                <div class="card-body">
-                  <img alt="Project-Dashboard" src="{{ asset('images/Project-Tasks.png') }}" class="centered-img"></img>
-                  <img alt="User-Dashboard" src="{{ asset('images/User-Dashboard.png') }}" class="centered-img"></img>
-                  <img alt="Organisation-Dashboard" src="{{ asset('images/Organisation-Dashboard.png') }}" class="centered-img"></img>
-                </div>
-              </div>
-            </div>
-          </div>
-          
-          <div class="pt-3 row justify-content-center">
-            <div class="col-md-12">
-              <div class="card">
-
-                <div class="card-header">A Propos</div>
-                <div class="card-body">
-                    <p>Cette application a été développée durant le semestre d'automne 2018 à la HE-Arc Ingénierie de Neuchâtel, dans le cadre du cours de développement web.</p>
-                    <p>Les personnes ayant contribué au projet sont:</p>
-                    <ul>
-                      <li>M. Bastien Wermeille</li>
-                      <li>M. Lucas Bulloni</li>
-                      <li>M. Malik Fleury</li>
-                    </ul>
-                </div>
-
-            </div>
-        </div>
-    </div>
+         </div>
+      </div>
+   </div>
 </div>
 @endsection
+
